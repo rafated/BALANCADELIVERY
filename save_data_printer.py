ee04968d876251937b50d8357fa68066304a00fb
Matheus Castro Maciel Lopes Marciano <100362000+MCMLopesM@users.noreply.github.com>
2024-08-08 21:18:11 +0100

    Update save_data_printer.py (adição da def main)

--- save_data_printer.py
+++ save_data_printer.py
@@ -6,7 +6,7 @@ import config
 
 count = 1
 
-#Abre porta serial de comunicacao arduino
+# abre porta serial de comunicacao arduino
 def open_serial_connection():
     arduino = None
     try:
@@ -15,13 +15,12 @@ def open_serial_connection():
         print(f'Erro ao carregar porta Arduino - {config.port_com_arduino}: {e}')
     return arduino
 
-#Retorna uma string com a data e hora de execução da função
+
 def get_string_time():
     current_time = datetime.datetime.now()
     time_string = current_time.strftime('%Y%m%d_%Hh%Mm%Ss')
     return time_string
 
-#Filtra e armazena os dados recebidos pela conexão serial
 def process_data(data, recibo, count, config):
     # Remover os caracteres '\x00' da string e rstrip para remover espaços à direita
     data1 = str(data).replace('\\x00', '')
@@ -33,7 +32,6 @@ def process_data(data, recibo, count, config):
         time_string = get_string_time()
 
         # Escrever no arquivo
-        #URGENTE Colocar em uma função também (rotina repetida)
         with open(config.temp_file_dir + "\\" + time_string + str(count) + ".txt", "a") as file_object:
             for line in recibo:
                 file_object.write(line + '\n')  # Salva os dados em um arquivo
@@ -46,7 +44,6 @@ def process_data(data, recibo, count, config):
         time_string = get_string_time()
 
         # Escrever no arquivo
-        #URGENTE Colocar em uma função também (rotina repetida)
         with open(config.temp_file_dir + "\\" + time_string + str(count) + ".txt", "a") as file_object:
             for line in recibo:
                 file_object.write(line + '\n')  # Salva os dados em um arquivo
@@ -55,37 +52,41 @@ def process_data(data, recibo, count, config):
     
     return recibo, count
 
-try:# para fazer registo em log dos possíveis erros
-    arduino = open_serial_connection()
+def main():
+    try:# para fazer registo em log dos possíveis erros
+        arduino = open_serial_connection()
 
-    time_string = get_string_time()
-    print(time_string)
-    
-    recibo=["A"]
-   
-    while(True):
-        #try caso o usb arduino seja desconectado não encera o programa
-        try:
-            while arduino.in_waiting:  # Or: while ser.inWaiting():
-                
-                recibo, count = process_data(data, recibo, count, config)                            
+        time_string = get_string_time()
+        print(time_string)
         
-        except Exception as e:
-            print(e)
-            print ('Erro ao ler o Arduino')
-            time.sleep(5) # Espera 5 segundos para que a coneçºao ao arduino seja realizada
-            try:
-                arduino.close()  #fecha ligacao serial com arduino
-                arduino = serial.Serial(port=config.port_com_arduino, baudrate=9600, timeout=.1)
-            except:
-                print ('erro load port arduino - '+config.port_com_arduino)
-            
+        recibo=["A"]
     
-    x = input() # para não fechar automaticamente
+        while(True):
+            #try caso o usb arduino seja desconectado não encera o programa
+            try:
+                while arduino.in_waiting:  # Or: while ser.inWaiting():
+                    
+                    recibo, count = process_data(data, recibo, count, config)                            
+            
+            except Exception as e:
+                print(e)
+                print ('Erro ao ler o Arduino')
+                time.sleep(5) # Espera 5 segundos para que a coneçºao ao arduino seja realizada
+                try:
+                    arduino.close()  #fecha ligacao serial com arduino
+                    arduino = serial.Serial(port=config.port_com_arduino, baudrate=9600, timeout=.1)
+                except:
+                    print ('erro load port arduino - '+config.port_com_arduino)
+                
+        
+        x = input() # para não fechar automaticamente
 
-except Exception as e:
-    print(e)
-    logf = open("Erro_Log.log", "a")
-    string_time = get_string_time()
-    logf.write(string_time + "; " + str(e) + '\n')
-    logf.close()
+    except Exception as e:
+        print(e)
+        logf = open("Erro_Log.log", "a")
+        string_time = get_string_time()
+        logf.write(string_time + "; " + str(e) + '\n')
+        logf.close()
+
+if __name__ == "__main__":
+    main()
