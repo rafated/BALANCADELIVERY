03171fdf2388159c721f56f9a9d7bb0e7593e469
Matheus Castro Maciel Lopes Marciano <100362000+MCMLopesM@users.noreply.github.com>
2024-08-08 18:35:08 +0100

    Refatoração save_data_printer.py (Separação de rotinas em funções e pequenas otimizações)
    Não testado em ambiente real, pequenas modificações podem ser necessárias

--- save_data_printer.py
+++ save_data_printer.py
@@ -4,72 +4,75 @@ import serial
 import sys
 import config
 
+count = 1
 
-arduino = ''
-count=1
-
-print("Save_data_printer.py")
-
-try:# para fazer registo em log dos possíveis erros
-
-    # abre porta serial de comunicacao arduino
+#Abre porta serial de comunicacao arduino
+def open_serial_connection():
+    arduino = None
     try:
         arduino = serial.Serial(port=config.port_com_arduino, baudrate=9600, timeout=.1)
-    except :
-        print ('erro load port arduino - '+config.port_com_arduino)
-        #sys.exit(1)
-    
+    except Exception as e:
+        print(f'Erro ao carregar porta Arduino - {config.port_com_arduino}: {e}')
+    return arduino
 
-    # teste string data e hora
-    t = datetime.datetime.now()
-    s=t.strftime('%Y%m%d_%Hh%Mm%Ss')
-    print(s)
+#Retorna uma string com a data e hora de execução da função
+def get_string_time():
+    current_time = datetime.datetime.now()
+    time_string = current_time.strftime('%Y%m%d_%Hh%Mm%Ss')
+    return time_string
+
+#Filtra e armazena os dados recebidos pela conexão serial
+def process_data(data, recibo, count, config):
+    # Remover os caracteres '\x00' da string e rstrip para remover espaços à direita
+    data1 = str(data).replace('\\x00', '')
+    print(str(data).rstrip())
     
+    # Checar pelo primeiro padrão
+    if "\\x1bd\\x1dVB\\r" in data1:    
+        print('Padrão encontrado')
+        time_string = get_string_time()
+
+        # Escrever no arquivo
+        #URGENTE Colocar em uma função também (rotina repetida)
+        with open(config.temp_file_dir + "\\" + time_string + str(count) + ".txt", "a") as file_object:
+            for line in recibo:
+                file_object.write(line + '\n')  # Salva os dados em um arquivo
+        recibo = [line]
+        count += 1
+    
+    # Checar pelo segundo padrão
+    elif "\\x1bd" in data1:    
+        print('Padrão encontrado')
+        time_string = get_string_time()
+
+        # Escrever no arquivo
+        #URGENTE Colocar em uma função também (rotina repetida)
+        with open(config.temp_file_dir + "\\" + time_string + str(count) + ".txt", "a") as file_object:
+            for line in recibo:
+                file_object.write(line + '\n')  # Salva os dados em um arquivo
+        recibo = []
+        count += 1
+    
+    return recibo, count
+
+try:# para fazer registo em log dos possíveis erros
+    arduino = open_serial_connection()
+
+    time_string = get_string_time()
+    print(time_string)
     
     recibo=["A"]
    
-     
     while(True):
-        
         #try caso o usb arduino seja desconectado não encera o programa
         try:
             while arduino.in_waiting:  # Or: while ser.inWaiting():
-
-                data = arduino.readline()# ler os dados enviados pelo arduino pela porta
-                recibo.append(str(data))
-                                
-                data1=str(data).replace('\\x00','')
-                print(str(data).rstrip())
-                #if "\\x1dVB" in str(data1):
-                #if "\\x1bd" in str(data1):
-                if "\\x1bd\\x1dVB\\r" in str(data1):    
-                    print('ok')
-                    t = datetime.datetime.now()
-                    s=t.strftime('%Y%m%d_%Hh%Mm_')
-                    
-                    with open(config.temp_file_dir+"\\"+s+str(count)+".txt", "a") as file_object:
-                        for line in recibo:
-                            file_object.write(line+'\n')  # guarda a data num ficheiro
-                    
-                    #recibo=[]
-                    recibo=[line]
-                    count=count+1
-                elif "\\x1bd" in str(data1):    
-                    print('ok')
-                    t = datetime.datetime.now()
-                    s=t.strftime('%Y%m%d_%Hh%Mm_')
-                    
-                    with open(config.temp_file_dir+"\\"+s+str(count)+".txt", "a") as file_object:
-                        for line in recibo:
-                            file_object.write(line+'\n')  # guarda a data num ficheiro
-                    
-                    #recibo=[]
-                    recibo=[]
-                    count=count+1
-                            
+                
+                recibo, count = process_data(data, recibo, count, config)                            
+        
         except Exception as e:
             print(e)
-            print ('erro ler arduino')
+            print ('Erro ao ler o Arduino')
             time.sleep(5) # Espera 5 segundos para que a coneçºao ao arduino seja realizada
             try:
                 arduino.close()  #fecha ligacao serial com arduino
@@ -80,10 +83,9 @@ try:# para fazer registo em log dos possíveis erros
     
     x = input() # para não fechar automaticamente
 
-except Exception as e:     # most generic exception you can catch
+except Exception as e:
     print(e)
     logf = open("Erro_Log.log", "a")
-    t = datetime.datetime.now()
-    s=t.strftime('%Y%m%d_%Hh%Mm%Ss')
-    logf.write(s+"; "+str(e)+'\n')
+    string_time = get_string_time()
+    logf.write(string_time + "; " + str(e) + '\n')
     logf.close()
