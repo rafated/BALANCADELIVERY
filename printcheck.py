42469a22cf8b0805a5d995df0cbca7e02f633a34
rafated <rrs_0104@hotmail.com>
2024-09-09 18:24:10 +0100

    Update printcheck.py
    Alterado layout para layout semi_final. inclusão da variavel global order_number para inserção no programa gui_pesagem

--- printcheck.py
+++ printcheck.py
@@ -4,12 +4,11 @@ import usb.util
 from datetime import datetime
 
 
-
 #README
 #Para correr este programa é necessário garantir que:
-# - Existe o ficheiro libusb.dll na pasta System32 e SYSWOW64 na pasta principal do Windows
+# - Existe o ficheiro libusb.dll na pasta System32 e SYSWOW64 na pasta principal do Windows. --> Instalar o libusb usando o ZADIG é a mlehor solução.
 # - pacote pysub está instalado
-# - necessário reinstalar os drivers da impressora Epson, com o software ZADIG. Fazer replace driver pelo da libusb. 
+# - necessário reinstalar os drivers da impressora Epson, com o software ZADIG. Fazer replace driver pelo da libusb.  
 # - verificar se os endereços da impressora sºao os mesmos que os definidos abaixo
 
 
@@ -50,28 +49,50 @@ def print_confirmation(order_number):
         endpoint = 1
     
         # Initialize the printer
-        printer.write(endpoint, b'\x1b\x40')
+
+        printer.write(endpoint, b'\x1b\x32\x20')
     
         printer.write(endpoint, align_center)
-        printer.write(endpoint, b'Pedido pesado e confirmado\n\n')
-        printer.write(endpoint, b'electronicamente por um sistema\n\n')
-        printer.write(endpoint, b'de pesagem com balança e imagem.\n\n')
+
+        printer.write(endpoint, b' Pedido pesado e confirmado\n\n\n\n')
+        printer.write(endpoint, b'electronicamente por um sistema\n\n\n\n')
+        printer.write(endpoint, b'de pesagem com balanca e imagem.\n\n\n\n')
         
         # Numero do pedido
     
         printer.write(endpoint, double_height_width)  # Large size
         printer.write(endpoint, bold_on)
-        printer.write(endpoint, b'Pedido número:' message.encode('utf-8'))
+        printer.write(endpoint, b'\n\n\n\n\n\n Pedido numero:')
+        printer.write(endpoint, message.encode('utf-8'))
         printer.write(endpoint, normal_size)  # Normal size
         printer.write(endpoint, bold_off)
 
         printer.write(endpoint, align_left)
-        printer.write(endpoint, b'\n\n\ Validado a:')
+        printer.write(endpoint, b'\n\n\n\n\n\n\n Validado a: ')
         printer.write(endpoint, dt_string.encode('utf-8'))
-        printer.write(endpoint, b'\n\n\\n\n')
+        printer.write(endpoint, b'\n\n\n\n \n\n\n\n')
+        printer.write(endpoint, b'\n\n\n\n \n\n\n\n')
+        printer.write(endpoint, b'\n\n\n\n \n\n\n\n')
+
     
         # Cut the paper
         printer.write(endpoint, b'\x1d\x56\x01')
         
     except usb.core.USBError as e:
         print(f"Could not write to the printer: {e}")
+
+def main():
+
+    numero='A5B6T'
+    print_confirmation(numero)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"Logging error: {e}")
+        with open("Erro_Log.log", "a") as logf:
+            t = datetime.now()
+            s = t.strftime('%Y%m%d_%Hh%Mm%Ss')
+            logf.write(f"{s};Erro no GUI ;{str(e)}\n")
+    
