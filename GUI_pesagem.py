23b4689be8f87b24e1e88808d726a94479f5fa66
rafated <rrs_0104@hotmail.com>
2026-01-16 15:20:38 +0000

    Update GUI_pesagem.py

--- GUI_pesagem.py
+++ GUI_pesagem.py
@@ -833,7 +833,7 @@ def process_weighing(window, serial_scale, estimated_weight, order_number, camer
         deviation = actual_weight - estimated_weight
         print(f"Actual weight: {actual_weight}, Deviation: {deviation}")
         window['-Peso_r-'].update(str(actual_weight))
-        
+        confirm_order_api(order_number)  # Confirmar o pedido na API
         
         # Se o desvio estiver dentro da faixa aceitável, o pedido deve ser confirmado
         if -60 <= deviation <= 80:
@@ -845,7 +845,6 @@ def process_weighing(window, serial_scale, estimated_weight, order_number, camer
                 weighing_try += 1
                 print(f"{CYAN}Peso dentro da faixa aceitável. Confirmando pedido {order_number}.{RESET}")
                 update_confirmation_status(window, deviation)
-                confirm_order_api(order_number)  # Confirmar o pedido na API
                 print_confirmation(order_number) #imprime ticket na impressora com dados
                 
                 # Enviar os dados de pesagem para a API
@@ -1199,6 +1198,7 @@ if __name__ == "__main__":
         main()
     except Exception as e:
         log_error(e)
+
 
 
 
